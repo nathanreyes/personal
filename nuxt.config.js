1e49965adb466f0f71dda5097011ebe833524237
Nathan Reyes <nathanreyes@me.com>
2018-03-23 15:34:43 -0500

    Modify path for PurgeCSS

--- nuxt.config.js
+++ nuxt.config.js
@@ -89,7 +89,7 @@ module.exports = {
               path.join(__dirname, './pages/**/*.vue'),
               path.join(__dirname, './layouts/**/*.vue'),
               path.join(__dirname, './components/**/*.vue'),
-              path.join(__dirname, './content/**/*.md'),
+              path.join(__dirname, './content/posts/*.md'),
             ]),
             extractors: [
               {
