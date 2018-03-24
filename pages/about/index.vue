8e38ea51872ea1a200da9b597ceb6dfe66cc7c67
Nathan Reyes <nathanreyes@me.com>
2018-03-24 09:49:28 -0500

    Modify about page.

--- pages/about/index.vue
+++ pages/about/index.vue
@@ -2,14 +2,17 @@
   <div>
     <h2 class='text-center'>Hi. I'm Nathan.</h2>
     <p>
-      I love building software tools. It all began about 10 years ago when I started building awful Microsoft
-      Access databases at first company (ugh). That got me hooked into coding and soon moved into Windows development (WPF, Winforms, SQL Databases).
+      I love building software tools. About 10 years ago, I started building awful Microsoft Access databases at my first company (ugh).
+      That gateway drug got me hooked into coding and soon moved into Windows development (WPF, Winforms, SQL Databases).
     </p>
     <p>
-      For about 3 years, I dipped my toes in iOS development and released a budgeting app to the App Store.
+      For about 3 years, I dipped my toes in iOS development. I released and supported a budgeting app called Ration to the App Store.
     </p>
     <p>
       Now, I prefer to build things for people using Vue.js, Nuxt, Express, Feathersjs and more.
+      I'd say I have a heavy frontend focus with a good eye for design using Vue and React, along with enough backend experience to get the job done.
+    </p>
+    <p>
       If you would like me to build something for you, please feel free to contact me via email at <a href='mailto:nathanreyes@me.com'>nathanreyes@me.com</a>.
     </p>
   </div>
