b17251583134ae53341ca6bd58933675692a701d
Nathan Reyes <nathanreyes@me.com>
2018-03-24 09:54:03 -0500

    Modify extractors

--- nuxt.config.js
+++ nuxt.config.js
@@ -96,7 +96,7 @@ module.exports = {
             extractors: [
               {
                 extractor: TailwindExtractor,
-                extensions: ['vue'],
+                extensions: ['vue', 'md'],
               },
             ],
             whitelist: ['html', 'body', 'nuxt-progress'],
