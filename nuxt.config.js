a91776539a53abe3f7d7e3ae02e3af00baad3f17
Nathan Reyes <nathanreyes@me.com>
2018-03-24 09:34:05 -0500

    Revert purge setting

--- nuxt.config.js
+++ nuxt.config.js
@@ -82,7 +82,7 @@ module.exports = {
           exclude: /(node_modules)/,
         });
       }
-      if (isDev) {
+      if (!isDev) {
         config.plugins.push(
           new PurgecssPlugin({
             // purgecss configuration
